ce8b6ac9e0fce93b2f81b3121f425c28ba130eb7
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-11-22 14:05:03 -0700

    taking unnecessary lines out

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -35,23 +35,3 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
-
-// const { GraphQLError } = require('graphql');
-// const jwt = require('jsonwebtoken');
-// require('dotenv');
-// // set token secret and expiration date
-// const secret = process.env.SECRET;
-// const expiration = process.env.EXPIRATION;
-
-// module.exports = {
-//   AuthenticationError: new GraphQLError('Could not authenticate user.', {
-//     extensions: {
-//       code: 'UNAUTHENTICATED',
-//     },
-//   }),
-//   signToken: function ({ username, email, _id }) {
-//     const payload = { username, email, _id };
-//     console.log(payload)
-//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-//   },
-// };
