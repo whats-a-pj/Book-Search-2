bcd1a1b2fa57b0f073b302e78eb8067ba86ac361
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-11-28 11:44:48 -0700

    fixed errors, won't know if saved works til login/signup are fixed though

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import {
   Container,
@@ -26,9 +26,9 @@ const SearchBooks = () => {
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
-  // useEffect(() => {
-  //   return () => saveBookIds(savedBookIds);
-  // });
+  useEffect(() => {
+    return () => saveBookIds(savedBookIds);
+  });
 
 
   // create method to search for books and set state on form submit
@@ -77,14 +77,9 @@ const SearchBooks = () => {
 
     try {
       const {data} = await saveBook({variables: {bookData: {...bookToSave}},});
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
+      } catch (err) {
       console.error(err);
     }
   };
